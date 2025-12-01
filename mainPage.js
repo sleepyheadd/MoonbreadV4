d868b9c590f1c8e36f1040b620fad63537379f4f
SP17-RedChromePlugin <92340125+sleepyheadd@users.noreply.github.com>
2025-12-01 16:23:49 -0500

    Update mainPage.js

--- mainPage.js
+++ mainPage.js
@@ -31,14 +31,14 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-let i;
-let slides = document.getElementsByClassName("slides");
-if (n > slides.length) {slideIndex = 1}
-else if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-slides[slideIndex-1].style.display = "flex";
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    if (n > slides.length) {slideIndex = 1}
+    else if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "flex";
 } 
 
 //Tabs
